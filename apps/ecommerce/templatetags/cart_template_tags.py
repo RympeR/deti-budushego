060de639e9c760b4a4ffe6adb72a39ea88cc0122
George Rashkov <georg.rashkov@gmail.com>
2021-07-20 12:06:39 +0300

    cart hide

--- apps/ecommerce/templatetags/cart_template_tags.py
+++ apps/ecommerce/templatetags/cart_template_tags.py
@@ -1,6 +1,9 @@
 from apps.ecommerce.models import *
 from django import template
 from django.db.models import Q
+from apps.blog.models import *
+from apps.lessons.models import *
+from apps.users.models import *
 
 register = template.Library()
 
@@ -39,4 +42,32 @@ def cart_products(request):
             order_items = qs.items_order.all()
             products = [ item.product for item in order_items ]
             return products
+    return 0
+
+@register.filter
+def post_categories(request):
+    qs = PostCategory.objects.all()
+    if qs.exists():
+        return qs
+    return 0
+
+@register.filter
+def gallery_categories(request):
+    qs = GalleryCategory.objects.all()
+    if qs.exists():
+        return qs
+    return 0
+
+@register.filter
+def lesson_categories(request):
+    qs = LessonCategory.objects.all()
+    if qs.exists():
+        return qs
+    return 0
+
+@register.filter
+def events_categories(request):
+    qs = EventCategory.objects.all()
+    if qs.exists():
+        return qs
     return 0
