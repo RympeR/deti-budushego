0debf789d363c0334746c15f26b7ae2396fd4e00
George <42513185+RympeR@users.noreply.github.com>
2021-07-31 19:15:25 +0300

    robots fix

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +11,9 @@ urlpatterns = [
     path('', include('apps.blog.urls')),
     path('', include('apps.ecommerce.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
