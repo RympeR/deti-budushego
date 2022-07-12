0a5806dd5d9900446f9c97c9770dae4f4c042012
George <42513185+RympeR@users.noreply.github.com>
2022-07-12 22:14:41 +0300

    update transl;ation

--- core/settings.py
+++ core/settings.py
@@ -121,11 +121,11 @@ LOGOUT_REDIRECT_URL = '/'
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'uk'
 
 LANGUAGES = [
-    ('ru', 'Русский'),
     ('uk', 'Украинский'),
+    ('ru', 'Русский'),
 ]
 
 LOCALE_PATHS = [
