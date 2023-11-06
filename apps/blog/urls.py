7ffdc20d9f7d8fc5b24f3dbad502b2020d4baffc
George Rashkov <support@indago-dev.com>
2023-11-06 22:35:31 +0200

    urls update

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('posts/', PostList.as_view(), name='post_list'),
     path('posts/<slug:slug>', PostDetail.as_view(), name='post_detail'),
     path('posts-filtered/<slug:slug>', PostListFiltered.as_view(), name='post_detail_filtered'),
-    path('news/', NewsList.as_view(), name='news_list'),
-    path('news/<slug:slug>', NewsDetail.as_view(), name='news_detail'),
+    path('info/', NewsList.as_view(), name='news_list'),
+    path('info/<slug:slug>', NewsDetail.as_view(), name='news_detail'),
     path('gallery/', GalleryList.as_view(), name='gallery'),
 ]
