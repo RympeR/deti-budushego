57b042eaf3a748a8f3399749c14aaf86de54ad21
George Rashkov <support@indago-dev.com>
2026-08-29 16:39:37 +0300

    news info uopdate

--- core/request_exposer.py
+++ core/request_exposer.py
@@ -4,8 +4,7 @@ from apps.ecommerce import models
 def RequestExposerMiddleware(get_response):
     def middleware(request):
         models.exposed_request = request
-        response = get_response(request)
-        return response
+        return get_response(request)
     return middleware
 
 def RequestLangMiddleware(get_response):
@@ -14,7 +13,6 @@ def RequestLangMiddleware(get_response):
             request.session['lang'] = 'uk'
         if request.session.get('django_language') is None:
             request.session['django_language'] = 'uk'
-        response = get_response(request)
-        return response
+        return get_response(request)
     return middleware
 
